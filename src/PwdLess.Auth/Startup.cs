256c814751e1d63137d379703db96eff8633d7c8
Biarity <biarity@outlook.com>
2016-12-31 12:38:10 +1000

    Set appsetting.json to not be optional

--- src/PwdLess.Auth/Startup.cs
+++ src/PwdLess.Auth/Startup.cs
@@ -20,8 +20,8 @@ namespace PwdLess.Auth
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                //.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
