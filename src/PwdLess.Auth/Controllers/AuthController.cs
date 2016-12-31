ba145304c834396c220b0ebd32ae40531c720a23
Biarity <biarity@outlook.com>
2016-12-31 14:30:02 +1000

    Added logging to AuthController

--- src/PwdLess.Auth/Controllers/AuthController.cs
+++ src/PwdLess.Auth/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using PwdLess.Auth.Services;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,16 +20,19 @@ namespace PwdLess.Auth.Controllers
         private ISenderService _senderService;
         private ITemplateProcessor _templateProcessor;
         private IDistributedCache _cache;
+        private ILogger _logger;
 
         public AuthController(IAuthService authService, 
             ISenderService senderService, 
             ITemplateProcessor templateProcessor, 
-            IDistributedCache cache)
+            IDistributedCache cache,
+            ILogger<AuthController> logger)
         {
             _authService = authService;
             _senderService = senderService;
             _templateProcessor = templateProcessor;
             _cache = cache;
+            _logger = logger;
         }
         
         public async Task<IActionResult> SendTotp(string identifier)
@@ -44,10 +48,12 @@ namespace PwdLess.Auth.Controllers
                 // send message user
                 await _senderService.SendAsync(identifier, body);
 
+                _logger.LogDebug($"A message was sent to: {identifier}. It contained the body: {body}");
                 return Ok($"Success! Sent TOTP to: {identifier}");
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                _logger.LogError(e.ToString());
                 return BadRequest("Something went wrong.");
             }
             
@@ -59,14 +65,18 @@ namespace PwdLess.Auth.Controllers
             {
                 // Get a TOTP's associated token
                 var token = await _authService.GetTokenFromTotp(totp);
+
+                _logger.LogDebug("TOTP: {totp}, token sent: {token}");
                 return Ok(token);
             }
             catch (IndexOutOfRangeException)
             {
+                _logger.LogDebug($"A requested TOTP's token was not found. TOTP: {totp}");
                 return NotFound("TOTP not found.");
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                _logger.LogError(e.ToString());
                 return BadRequest("Something went wrong.");
             }
 
@@ -74,6 +84,7 @@ namespace PwdLess.Auth.Controllers
         
         public IActionResult Echo(string echo) // for testing
         {
+            _logger.LogDebug($"Echoed: {echo}");
             return Content(echo);
         }
     }
