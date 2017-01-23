a2c09b5f054eadea4b585386ca7b7841cd6d9f9d
Biarity <biarity@outlook.com>
2017-01-24 09:05:23 +1000

    Added ActionService class

---
+++ src/PwdLess/Services/ActionService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PwdLess.Auth.Services
+{
+    /// <summary>
+    /// Handles running the HTTP Actions defined in configuration.
+    /// </summary>
+    public interface IActionService
+    {
+        Task<string> BeforeSendingNonce(string identifier);
+        Task<string> BeforeSendingToken(string nonce);
+    }
+
+    public class ActionService
+    {
+    }
+}
